889242f413a95be5b6fe63df5e07f0d7743adedf
ckstn123 <ckstn123@github.com>
2023-12-10 16:42:09 +0900

    var.go, module create

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ckstn123/Go-module
+
+go 1.21.4
---
+++ var1.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int = 1
+	var b string = "Hi"
+
+	fmt.Println(a)
+	fmt.Println(b)
+}
